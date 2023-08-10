740f0c3c33478c04e2cc598d2c64cbbdad9baf44
agustintmm <agustinmartinez2003@gmail.com>
2023-08-10 13:22:20 -0300

    update viewsets ordering

--- drfcrudapp/api.py
+++ drfcrudapp/api.py
@@ -1,18 +1,24 @@
 from .models import *
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, filters
 from .serializers import *
 
 class ProductoViewSet(viewsets.ModelViewSet):
     queryset = Producto.objects.all()
     permission_classes = [permissions.AllowAny]
     serializer_class = ProductoSerializer
+    filter_backends = [filters.OrderingFilter]
+    ordering = ['id']
     
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     permission_classes = [permissions.AllowAny]
     serializer_class = PostSerializer
+    filter_backends = [filters.OrderingFilter]
+    ordering = ['id']
     
 class CursoViewSet(viewsets.ModelViewSet):
     queryset = Curso.objects.all()
     permission_classes = [permissions.AllowAny]
     serializer_class = CursoSerializer
+    filter_backends = [filters.OrderingFilter]
+    ordering = ['id']
