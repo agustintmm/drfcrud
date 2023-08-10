a7bcb4100309eaf972ffe96877caef7727abb90e
agustintmm <agustinmartinez2003@gmail.com>
2023-08-10 12:06:01 -0300

    updating permissions classes

--- drfcrudapp/api.py
+++ drfcrudapp/api.py
@@ -4,15 +4,15 @@ from .serializers import *
 
 class ProductoViewSet(viewsets.ModelViewSet):
     queryset = Producto.objects.all()
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [permissions.AllowAny]
     serializer_class = ProductoSerializer
     
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [permissions.AllowAny]
     serializer_class = PostSerializer
     
 class CursoViewSet(viewsets.ModelViewSet):
     queryset = Curso.objects.all()
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [permissions.AllowAny]
     serializer_class = CursoSerializer
